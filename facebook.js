e1c4813ef6dfee8a2d5b5a70c8b17e723a136ac8
Rob Graeber <bgraeber@gmail.com>
2014-04-13 03:10:37 -0700

    A bit of promise refactoring, facebook events just need to be added to db now

--- facebook.js
+++ facebook.js
@@ -12,6 +12,7 @@ var insertCount = 0;
 var terminateTimer;
 var locationNames = [];
 var eventIds = [];
+var events = [];
 //need to manually terminate program when scraping complete, since db connection is always open
 var terminateProgram = function(){
   console.log("Program finished,", insertCount, "entries added / updated");
@@ -26,11 +27,14 @@ var refreshTerminateTimer = function(){
   terminateTimer = setTimeout(terminateProgram, 30*1000);
 };
 //gets all events at the location and adds id to eventIds
-var getEventByLocationName = function(name){
+var getEventIdsByLocationName = function(name){
   var url = "https://graph.facebook.com/search?q="+name+"&type=event&access_token="+facebookApiKey;
   return request.getAsync(url)
-  .then(function(args){
-    var body = JSON.parse(args[0].body);
+  .spread(function(res, body){
+    body = JSON.parse(body);
+    if(!body.data){
+      throw "getEventIdsByLocationName - "+ JSON.stringify(body);
+    }
     eventIds = eventIds.concat(_.pluck(body.data, "id"));
   });
 };
@@ -43,8 +47,8 @@ var getPlaces = function(apiURL, recursiveCount, finishCallback){
 
   console.log("Trying to get location results from:", apiURL);
   request.getAsync(apiURL)
-  .then(function(args){
-    var body = JSON.parse(args[0].body);
+  .spread(function(res, body){
+    body = JSON.parse(body);
     var results = body.data;
     body.paging = body.paging || {};
     nextUrl = body.paging.next;
@@ -53,7 +57,7 @@ var getPlaces = function(apiURL, recursiveCount, finishCallback){
       console.log("Results length:", results.length);
       return results;
     }else{
-      throw "Results Undefined Error - "+JSON.stringify(body.error);
+      throw "getPlaces - "+JSON.stringify(body);
     }
   })
   .then(function (results){
@@ -61,32 +65,40 @@ var getPlaces = function(apiURL, recursiveCount, finishCallback){
     return results;
   })
   .then(function (results){
-    console.log("Total location Ids:", locationNames.length);
     if(recursiveCount > 1 && nextUrl){
       getPlaces(nextUrl,recursiveCount-1, finishCallback);
     }else{
+      console.log("Total location Ids:", locationNames.length);
       finishCallback(locationNames);
     }
-  })
-  .catch(function(err){
-    console.log("ERR:", err);
   });
 };
+var getPlacesAsync = function(apiURL, recursiveCount){
+  return new Promise(function(resolve,reject){
+    getPlaces(apiURL, recursiveCount, resolve);
+  });
+};
+//takes an array of event id, then builds the giant FQL query url. Can take upto 2000 event id's at a time.
 var getEvents = function(ids){
   var eids = _.map(ids, function(id){
      return "eid = "+id; 
   });
   eids = eids.join(" OR ");
 
-  console.log("EIDS:", eids);
+  // console.log("EIDS:", eids);
   var url = "https://graph.facebook.com/fql?q=SELECT name,description,attending_count,eid,start_time,end_time,location,venue,ticket_uri FROM event WHERE "+eids+"&access_token="+facebookApiKey;
   
-  request.getAsync(url)
-  .then(function(args){
-    var body = JSON.parse(args[0].body);
-    body.data = body.data || {};
-    console.log(body);
-    console.log("Returned Events:", body.data.length);
+  return request.getAsync(url)
+  .spread(function(res, body){
+    body = JSON.parse(body);
+    if(!body.data){
+      throw "getEvents - "+JSON.stringify(body);
+    }
+    body.data = body.data || [];
+    
+    events = events.concat(body.data);
+    console.log("Received Events:", body.data.length);
+    return body.data;
   });
 };
 //splits arrays into smaller arrays based on targetLength, returns an array of arrays
@@ -99,41 +111,63 @@ var arraySplit = function(array, targetLength){
   return superArray;
 };
 
+//geo-codes the target address and starts the data gathering sequence
 request.getAsync({url:"https://maps.googleapis.com/maps/api/geocode/json", qs:{key:googleApiKey, sensor:"false", address:targetAddress}})
-.then(function(args){
-  var body = JSON.parse(args[0].body);
+.spread(function(res, body){
+  body = JSON.parse(body);
   if(body.status === "OK"){
     var lat = body.results[0].geometry.location.lat;
     var lon = body.results[0].geometry.location.lng;
+    console.log("Lat:", lat, "Long:", lon, "Status: OK");
     return {lat: lat, lon:lon};
   }else {
-    console.log("API Error:", body.status);
-    process.exit(1);
-    return null;
-  }})
-.then(function(data){
-  console.log("Lat:", data.lat, "Long:", data.lon, "Status: OK");
-  if(!data){
-    return;
+    throw "API Error: "+body.status;
   }
-  var currentDate = new Date().getTime()-24*60*60*1000; //currentTime minus 1 day;
+})
+.then(function(data){
   var url = "https://graph.facebook.com/search?q=*&type=place&center="+data.lat+","+data.lon+"&distance="+radius+"&access_token="+facebookApiKey;
+  return getPlacesAsync(url, 2);
+})
+.then(function(places){
+  var eventPromises = [];
+  console.log("Getting "+places.length+" EventIdsByLocationName..");
   
-  getPlaces(url, 2, function(places){
-    var promises = [];
-    _.each(places.slice(0,100), function(place){
-      promises.push(getEventByLocationName(place));
+  //delays to prevent denial of service
+  _.each(places, function(place, index){
+    var promise = Promise.delay(100*index).then(function(){
+      return getEventIdsByLocationName(place);
     });
-
-    Promise.all(promises)
-    .then(function(){
-      console.log("eventIds length:",eventIds.length);
-      getEvents(eventIds.slice(0,2500));
-    })
-    .catch(function(err){
-      console.log(err);
-      terminateProgram();
-    });
+    eventPromises.push(promise);
   });
+
+  //populates global eventIds array
+  return Promise.all(eventPromises);
+})
+.then(function(){
+  var eventPromises = [];
+  var superArray = arraySplit(eventIds, 1000);
+  console.log("eventIds length:",eventIds.length);
+
+  //delays to prevent denial of service
+  _.each(superArray, function(eids, index){
+    var promise = Promise.delay(100*index).then(function(){
+      return getEvents(eids);
+    });
+    eventPromises.push(promise);
+  });
+
+  //populates global events array
+  return Promise.all(eventPromises);
+})
+.then(function(){
+  console.log("Total Events:", events.length);
+  _.each(events, function(item){
+    //add to db
+  });
+  terminateProgram();
+})
+.catch(function(err){
+  console.log("ERR:", err);
+  terminateProgram();
 });
 
